1ba7c7db274d0a4488fee13425d42fb165d533a7
sguede <vinceguede@yahoo.fr>
2016-02-02 15:49:58 -0500

    Verser les fichiers initiaux

---
+++ script/main.js
@@ -0,0 +1,20 @@
+﻿
+
+
+
+window.onload = principale;
+
+function principale (){
+    console.log('Page chargée');
+    var boutons = document.getElementsByTagName('BUTTON');
+    console.log(boutons.length);
+
+    // Maintenant on peut brancher les listeners
+    boutons[0].onclick = ecrire;
+
+}
+
+
+function ecrire(){
+    console.log(this);
+}
