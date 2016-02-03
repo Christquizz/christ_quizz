ca1c570e93a79054de6eb90d974a66b12e5569b1
sguede <vinceguede@yahoo.fr>
2016-02-03 08:14:17 -0500

    Essai Vincent 3

--- script/main.js
+++ script/main.js
@@ -5,16 +5,22 @@
 window.onload = principale;
 
 function principale (){
+///**!/Pour masquer la division :
+    document.getElementById('debutant').style.display = none;
     console.log('Page chargée');
     var boutons = document.getElementsByTagName('BUTTON');
+
     console.log(boutons.length);
 
     // Maintenant on peut brancher les listeners
     boutons[0].onclick = ecrire;
-
 }
 
 
 function ecrire(){
     console.log(this);
 }
+
+
+///Pour afficher la division :
+//document.getElementById(identifiant_de_ma_div).style.display = block;
