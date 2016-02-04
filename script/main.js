f1d7f6c2bbb36b2f99ed104feb98291e3c3d1ac4
rmbaye <mbaye_racine@hotmail.com>
2016-02-03 22:22:36 -0500

    afficher & cacher des DIV

--- script/main.js
+++ script/main.js
@@ -2,25 +2,37 @@
 
 
 
-window.onload = principale;
+//window.onload = principale;
 
-function principale (){
-///**!/Pour masquer la division :
-    document.getElementById('debutant').style.display = none;
-    console.log('Page chargée');
-    var boutons = document.getElementsByTagName('BUTTON');
+//function principale (){
+/////**!/Pour masquer la division :
+//    document.getElementById('debutant').style.display = none;
+//    console.log('Page chargée');
+//    var boutons = document.getElementsByTagName('BUTTON');
+//
+//    console.log(boutons.length);
+//
+//    // Maintenant on peut brancher les listeners
+//    boutons[0].onclick = ecrire;
+//}
 
-    console.log(boutons.length);
 
-    // Maintenant on peut brancher les listeners
-    boutons[0].onclick = ecrire;
-}
-
-
-function ecrire(){
-    console.log(this);
-}
+//function ecrire(){
+//    console.log(this);
+//}
 
 
 ///Pour afficher la division :
-//document.getElementById(identifiant_de_ma_div).style.display = block;
+function afficher_cacher(id)
+{
+    if (document.getElementById(id).style.visibility == "hidden")
+    {
+        document.getElementById(id).style.visibility = "visible";
+        document.getElementById('niveau1_'+id).innerHTML = 'DEBUTANT';
+    }
+    else {
+        document.getElementById(id).style.visibility = "hidden";
+        document.getElementById('niveau1_'+id).innerHTML = 'DEBUTANT';
+    }
+    return true;
+}
