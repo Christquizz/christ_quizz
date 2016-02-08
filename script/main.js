3f26edf0a2f673288f6be5ccfaf48a44f5ea36e5
rmbaye <mbaye_racine@hotmail.com>
2016-02-08 14:33:38 -0500

    affichage "progress bar"

--- script/main.js
+++ script/main.js
@@ -34,7 +34,63 @@ function principale (){
     }
 }*/
 
+//JavaScript Progress Bar
+var debut_progress_bar = document.getElementById('debut');
+console.log(debut_progress_bar);
+debut_progress_bar.onclick = move;
 
+function move() {
+    var elem = document.getElementsByClassName("myBar");
+    var elem1 = elem[0];
+    var width = 0;
+    var id = setInterval(frame, 1000);
+    function frame() {
+        if (width == 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem1.style.width = width + '%';
+        }
+    }
+}
+
+var debut_progress_bar1 = document.getElementById('debut1');
+console.log(debut_progress_bar1);
+debut_progress_bar1.onclick = move1;
+
+function move1() {
+    var elem = document.getElementsByClassName("myBar");
+    var elem2 = elem[1];
+    var width = 0;
+    var id = setInterval(frame, 1000);
+    function frame() {
+        if (width == 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem2.style.width = width + '%';
+        }
+    }
+}
+
+var debut_progress_bar2 = document.getElementById('debut2');
+console.log(debut_progress_bar2);
+debut_progress_bar2.onclick = move2;
+
+function move2() {
+    var elem = document.getElementsByClassName("myBar");
+    var elem3 = elem[2];
+    var width = 0;
+    var id = setInterval(frame, 1000);
+    function frame() {
+        if (width == 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem3.style.width = width + '%';
+        }
+    }
+}
 
 
 
